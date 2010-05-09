48266997c1704b6794e2c30c32b35cdad37a2caa
Tariksin <tariksin@gmail.com>
2010-05-09 23:11:17 +0200

    Shitty and not so optimal fix for parsing code-blocks, but hey it works at least

--- lib/syntaxcolorize.rb
+++ lib/syntaxcolorize.rb
@@ -6,12 +6,16 @@ module Nanoc3::Filters
       require 'coderay'
       require 'nokogiri'
       doc = Nokogiri::HTML(content)
-      doc.css('pre code').each do |e|
-        # Get text and language
-        code = e.inner_text
-        lang = /language-([a-z0-9\-_]+)/.match(e['class']) || "ruby"
-        # Process
-        e.inner_html = ::CodeRay.scan(code, lang).html(params)
+      doc.css('pre').each do |e|
+        e.css("code").each do |e|
+          code = e.inner_text
+          e.inner_html = ::CodeRay.scan(code, "ruby").html(params)
+        end
+        e.css("code[@class*=language-]").each do |e|
+          code = e.inner_text
+          lang = /language-([a-z0-9\-_]+)/.match(e['class'])[1]
+          e.inner_html = ::CodeRay.scan(code, lang).html(params)
+        end
       end
       doc.to_s
     end
