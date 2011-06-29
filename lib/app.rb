c5076c0b5e2379b1fdfbdd00c06b414c7521d177
Nerian <siotopo@gmail.com>
2011-06-29 18:05:31 +0200

    Collapse the menu. Partials

--- lib/app.rb
+++ lib/app.rb
@@ -14,10 +14,18 @@ end
 
 get '/docs/?' do           
   haml :"docs/introduction.html", :layout => :'docs_layout'
+end
+
+get '/contributors' do
+  haml :"/contributors"
 end       
 
-get '/docs/*/?' do
-  
+get '/stories' do
+  haml :"/stories"
+end       
+
+
+get '/docs/*/?' do  
   haml :"docs/#{params[:splat].join('/')}.html", :layout => :'docs_layout'
 end          
 
@@ -27,4 +35,24 @@ end
 
 get '/coderay.css' do
   css :"scss/coderay"
+end
+
+
+module Haml
+  module Helpers
+    def partial(template, *args)
+      template_array = template.to_s.split('/')
+      template = template_array[0..-2].join('/') + "/_#{template_array[-1]}"
+      options = args.last.is_a?(Hash) ? args.pop : {}
+      options.merge!(:layout => false)
+      if collection = options.delete(:collection) then
+        collection.inject([]) do |buffer, member|
+          buffer << haml(:"#{template}", options.merge(:layout =>
+          false, :locals => {template_array[-1].to_sym => member}))
+        end.join("\n")
+      else
+        haml(:"#{template}", options)
+      end
+    end
+  end
 end
