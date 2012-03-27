cd175becd2e86330c38985c5b29720bde8091afa
Durran Jordan <durran@gmail.com>
2012-03-27 21:45:09 +0200

    Fix page nav to scroll and fix to top as well

--- source/javascripts/mongoid.js
+++ source/javascripts/mongoid.js
@@ -1,22 +1,50 @@
 $(function() {
-  var $win = $(window)
-    , $nav = $('.subnav')
-    , navTop = $('.subnav').length && $('.subnav').offset().top - 60
-    , isFixed = 0
 
-  processScroll();
+  var $win = $(window);
 
-  $win.on('scroll', processScroll);
+  var $subnav = $(".subnav");
+  var $pagenav = $(".page-nav");
 
-  function processScroll() {
-    var i, scrollTop = $win.scrollTop();
+  var subnavTop = $(".subnav").length && $(".subnav").offset().top - 60;
+  var pagenavTop = $(".page-nav").length && $(".page-nav").offset().top - 120;
 
-    if (scrollTop >= navTop && !isFixed) {
-      isFixed = 1;
-      $nav.addClass('subnav-fixed');
-    } else if (scrollTop <= navTop && isFixed) {
-      isFixed = 0;
-      $nav.removeClass('subnav-fixed');
+  var subnavFixed = 0;
+  var pagenavFixed = 0;
+
+  var scrollSubNav = function() {
+    var _, scrollTop = $win.scrollTop();
+
+    if (scrollTop >= subnavTop && !subnavFixed) {
+      subnavFixed = 1;
+      $subnav.addClass("subnav-fixed");
+    }
+    else if (scrollTop <= subnavTop && subnavFixed) {
+      subnavFixed = 0;
+      $subnav.removeClass("subnav-fixed");
     }
   }
+
+  var scrollPageNav = function() {
+    var _, scrollTop = $win.scrollTop();
+
+    console.log(pagenavTop);
+    console.log(scrollTop);
+
+    if (scrollTop >= pagenavTop && !pagenavFixed) {
+      console.log("add");
+      pagenavFixed = 1;
+      $pagenav.addClass("page-nav-fixed");
+    }
+    else if (scrollTop <= pagenavTop && pagenavFixed) {
+      console.log("remove")
+      pagenavFixed = 0;
+      $pagenav.removeClass("page-nav-fixed");
+    }
+  }
+
+  scrollSubNav();
+  scrollPageNav();
+
+  $win.bind("scroll", scrollSubNav);
+  $win.bind("scroll", scrollPageNav);
 });
