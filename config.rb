3b9349f0be18d909d1958f386cbb02e7a4de4b13
Durran Jordan <durran@gmail.com>
2012-05-17 16:34:41 +0200

    Namespace docs under en

--- config.rb
+++ config.rb
@@ -1,118 +1,118 @@
 Haml::Filters::CodeRay.encoder_options = { :css => :class }
 
-page "/mongoid/*", layout: "layouts/mongoid"
-page "/origin/*", layout: "layouts/origin"
-page "/moped/*", layout: "layouts/moped"
+page "/en/mongoid/*", layout: "en/layouts/mongoid"
+page "/en/origin/*", layout: "en/layouts/origin"
+page "/en/moped/*", layout: "en/layouts/moped"
 
 # We've revamped the entire set URL structure from the previous, so setup
 # the 301 redirects below.
 
 page "/" do
-  redirect "/mongoid/index.html", 301
+  redirect "/en/mongoid/index.html", 301
 end
 
 page "/index.html" do
-  redirect "/mongoid/index.html", 301
+  redirect "/en/mongoid/index.html", 301
 end
 
 page "/docs.html" do
-  redirect "/mongoid/docs/installation.html", 301
+  redirect "/en/mongoid/docs/installation.html", 301
 end
 
 page "/docs/installation.html" do
-  redirect "/mongoid/docs/installation.html", 301
+  redirect "/en/mongoid/docs/installation.html", 301
 end
 
 page "/docs/installation/*" do
-  redirect "/mongoid/docs/installation.html", 301
+  redirect "/en/mongoid/docs/installation.html", 301
 end
 
 page "/docs/documents.html" do
-  redirect "/mongoid/docs/documents.html", 301
+  redirect "/en/mongoid/docs/documents.html", 301
 end
 
 page "/docs/documents/*" do
-  redirect "/mongoid/docs/documents.html", 301
+  redirect "/en/mongoid/docs/documents.html", 301
 end
 
 page "/docs/persistence.html" do
-  redirect "/mongoid/docs/persistence.html", 301
+  redirect "/en/mongoid/docs/persistence.html", 301
 end
 
 page "/docs/persistence/*" do
-  redirect "/mongoid/docs/persistence.html", 301
+  redirect "/en/mongoid/docs/persistence.html", 301
 end
 
 page "/docs/querying.html" do
-  redirect "/mongoid/docs/querying.html", 301
+  redirect "/en/mongoid/docs/querying.html", 301
 end
 
 page "/docs/querying/*" do
-  redirect "/mongoid/docs/querying.html", 301
+  redirect "/en/mongoid/docs/querying.html", 301
 end
 
 page "/docs/relations.html" do
-  redirect "/mongoid/docs/relations.html", 301
+  redirect "/en/mongoid/docs/relations.html", 301
 end
 
 page "/docs/relations/*" do
-  redirect "/mongoid/docs/relations.html", 301
+  redirect "/en/mongoid/docs/relations.html", 301
 end
 
 page "/docs/identity_map.html" do
-  redirect "/mongoid/docs/identity_map.html", 301
+  redirect "/en/mongoid/docs/identity_map.html", 301
 end
 
 page "/docs/callbacks.html" do
-  redirect "/mongoid/docs/callbacks.html", 301
+  redirect "/en/mongoid/docs/callbacks.html", 301
 end
 
 page "/docs/callbacks/*" do
-  redirect "/mongoid/docs/callbacks.html", 301
+  redirect "/en/mongoid/docs/callbacks.html", 301
 end
 
 page "/docs/validation.html" do
-  redirect "/mongoid/docs/validation.html", 301
+  redirect "/en/mongoid/docs/validation.html", 301
 end
 
 page "/docs/indexing.html" do
-  redirect "/mongoid/docs/indexing.html", 301
+  redirect "/en/mongoid/docs/indexing.html", 301
 end
 
 page "/docs/extras.html" do
-  redirect "/mongoid/docs/extras.html", 301
+  redirect "/en/mongoid/docs/extras.html", 301
 end
 
 page "/docs/rails.html" do
-  redirect "/mongoid/docs/rails.html", 301
+  redirect "/en/mongoid/docs/rails.html", 301
 end
 
 page "/docs/rails/*" do
-  redirect "/mongoid/docs/rails.html", 301
+  redirect "/en/mongoid/docs/rails.html", 301
 end
 
 page "/docs/extensions.html" do
-  redirect "/mongoid/index.html", 301
+  redirect "/en/mongoid/index.html", 301
 end
 
 page "/docs/upgrading.html" do
-  redirect "/mongoid/docs/upgrading.html", 301
+  redirect "/en/mongoid/docs/upgrading.html", 301
 end
 
 page "/docs/contributing.html" do
-  redirect "/mongoid/docs/contributing.html", 301
+  redirect "/en/mongoid/docs/contributing.html", 301
 end
 
 page "/performance.html" do
-  redirect "/mongoid/docs/performance.html", 301
+  redirect "/en/mongoid/docs/performance.html", 301
 end
 
 page "/credits.html" do
-  redirect "/mongoid/links.html", 301
+  redirect "/en/mongoid/links.html", 301
 end
 
 page "/links.html" do
-  redirect "/mongoid/links.html", 301
+  redirect "/en/mongoid/links.html", 301
 end
 
 configure :build do
