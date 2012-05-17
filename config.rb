743262db4c91f4b97314e7eb5d92e9797ce788c6
Durran Jordan <durran@gmail.com>
2012-05-17 19:50:27 +0200

    Force default external encoding to utf-8

--- config.rb
+++ config.rb
@@ -1,5 +1,12 @@
 # encoding: utf-8
+Encoding.default_external = "utf-8"
+
 Haml::Filters::CodeRay.encoder_options = { :css => :class }
+Haml::Template.options[:encoding] = "utf-8"
+
+page "/en/mongoid/*", layout: "en/layouts/mongoid"
+page "/en/origin/*", layout: "en/layouts/origin"
+page "/en/moped/*", layout: "en/layouts/moped"
 
 page "/en/mongoid/*", layout: "en/layouts/mongoid"
 page "/en/origin/*", layout: "en/layouts/origin"
@@ -124,3 +131,6 @@ end
 ignore "/en/layouts/mongoid.haml"
 ignore "/en/layouts/moped.haml"
 ignore "/en/layouts/origin.haml"
+ignore "/zh-cn/layouts/mongoid.haml"
+ignore "/zh-cn/layouts/moped.haml"
+ignore "/zh-cn/layouts/origin.haml"
