9125666bb7901e536e07780f2ba974e7550c2d9c
Durran Jordan <durran@gmail.com>
2012-07-10 11:25:04 +0200

    Update to middleman 3.0

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source :rubygems
 gem "haml-coderay"
-gem "heroku"
-gem "middleman", "3.0.0.beta.3"
+gem "middleman", "~> 3.0.0"
 gem "middleman-smusher"
--- config.rb
+++ config.rb
@@ -2,7 +2,6 @@
 Encoding.default_external = "utf-8"
 
 Haml::Filters::CodeRay.encoder_options = { :css => :class }
-Haml::Template.options[:encoding] = "utf-8"
 
 page "/en/mongoid/*", layout: "en/layouts/mongoid"
 page "/en/origin/*", layout: "en/layouts/origin"
@@ -18,10 +17,3 @@ ignore "/en/layouts/origin.haml"
 ignore "/zh-cn/layouts/mongoid.haml"
 ignore "/zh-cn/layouts/moped.haml"
 ignore "/zh-cn/layouts/origin.haml"
-
-helpers do
-
-  def header_class(section)
-    request.path =~ /(docs|links)/i ? "basic" : section
-  end
-end
